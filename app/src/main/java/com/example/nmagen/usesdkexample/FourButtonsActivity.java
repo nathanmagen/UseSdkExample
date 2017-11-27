6aae65067a06f3fed3aecc607aa37b817c2fd8e3
nmagen <nathanmagen@mobiletornado.com>
2017-11-27 09:53:28 +0200

    Added new activity with four buttons that would be the functionality of the app

---
+++ app/src/main/java/com/example/nmagen/usesdkexample/FourButtonsActivity.java
@@ -0,0 +1,13 @@
+package com.example.nmagen.usesdkexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FourButtonsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_four_buttons);
+    }
+}
