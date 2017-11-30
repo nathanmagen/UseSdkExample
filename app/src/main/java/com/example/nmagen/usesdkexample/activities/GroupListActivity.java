c06aae4383f2678a969fb111c955cbc7e56201f6
nmagen <nathanmagen@mobiletornado.com>
2017-11-30 15:06:28 +0200

    Added OnClickListener for the group list, still can't figure out how to disable the click option to push the ptt button easily

--- app/src/main/java/com/example/nmagen/usesdkexample/activities/GroupListActivity.java
+++ app/src/main/java/com/example/nmagen/usesdkexample/activities/GroupListActivity.java
@@ -2,10 +2,13 @@ package com.example.nmagen.usesdkexample.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import com.MobileTornado.sdk.model.data.Group;
 import com.example.nmagen.usesdkexample.adapters.ListToViewAdapter;
 import com.example.nmagen.usesdkexample.R;
 import com.example.nmagen.usesdkexample.data.AppGroup;
@@ -13,6 +16,8 @@ import com.example.nmagen.usesdkexample.presenters.GroupPresenter;
 import com.example.nmagen.usesdkexample.presenters.PresentersManager;
 
 import java.util.List;
+
+import listeners.RecyclerTouchListener;
 
 public class GroupListActivity extends AppCompatActivity {
     private PresentersManager presentersManager = PresentersManager.getInstance();
@@ -30,7 +35,48 @@ public class GroupListActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.group_list_view);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
         adapter = new ListToViewAdapter(groupList);
         recyclerView.setAdapter(adapter);
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                AppGroup clickedGroup = groupList.get(position);
+                if (!clickedGroup.isSelected()) {
+                    // select procedure, consider moving to private method
+                    clickedGroup.setSelected();
+                    view.findViewById(R.id.pttButton).setEnabled(true);
+                    view.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                }
+                else {
+                    // un select procedure, consider moving to private method
+                    clickedGroup.setUnSelected();
+                    view.findViewById(R.id.pttButton).setEnabled(false);
+                    view.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                    view.setClickable(true);
+                }
+                int listSize = groupList.size();
+                for (int i = 0; i < listSize; i++) {
+                    if (position != i) {
+                        groupList.get(i).setUnSelected();
+                        View v = recyclerView.getChildAt(i);
+                        v.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                        v.findViewById(R.id.pttButton).setEnabled(false);
+                        v.setClickable(true);
+                    }
+                }
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+    }
+
+    public void onPttClick(View view) {
+        int pos = (int) view.getTag();
+        AppGroup ag = groupList.get(pos);
+        Toast.makeText(this, "PTT of " + ag.getGroup().getDisplayName(), Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/nmagen/usesdkexample/adapters/ListToViewAdapter.java
+++ app/src/main/java/com/example/nmagen/usesdkexample/adapters/ListToViewAdapter.java
@@ -2,10 +2,11 @@ package com.example.nmagen.usesdkexample.adapters;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
-import com.MobileTornado.sdk.model.data.Group;
 import com.example.nmagen.usesdkexample.R;
 import com.example.nmagen.usesdkexample.data.AppGroup;
 
@@ -19,10 +20,12 @@ public class ListToViewAdapter extends RecyclerView.Adapter {
     private List<AppGroup> groupList;
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView textView;
-        ViewHolder(TextView tv) {
-            super(tv);
-            textView = tv;
+        public TextView groupNameView;
+        public Button pttButton;
+        ViewHolder(View v) {
+            super(v);
+            groupNameView = v.findViewById(R.id.groupName);
+            pttButton = v.findViewById(R.id.pttButton);
         }
     }
 
@@ -32,15 +35,16 @@ public class ListToViewAdapter extends RecyclerView.Adapter {
 
     @Override
     public ListToViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        TextView v = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.support_simple_spinner_dropdown_item, parent, false);
-        ViewHolder vh = new ViewHolder(v);
-        return vh;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.group_list_line, parent, false);
+        return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         ViewHolder lHolder = (ViewHolder)holder;
-        lHolder.textView.setText(groupList.get(position).getGroup().getDisplayName());
+        lHolder.groupNameView.setText(groupList.get(position).getGroup().getDisplayName());
+        lHolder.pttButton.setTag(position);
+        lHolder.pttButton.setEnabled(false);
     }
 
     @Override
